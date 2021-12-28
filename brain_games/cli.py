835c0bf5afb643daad5c0652a9ee92b536e3bf48
Lev Bazarnov <16255087+lbazarnov@users.noreply.github.com>
2021-12-28 11:09:40 +0500

    Исправления в файле cli.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,9 +1,9 @@
-from brain_games.engine import get_user_name
+import prompt
 
 
 def welcome_user():
     """Asks user for his name and greets him"""
-    user_name = get_user_name()
+    print('Welcome to the Brain Games!')
+    user_name = prompt.string('May I have your name? ')
     greeting = f'Hello, {user_name}!'
     print(greeting)
-    return user_name
